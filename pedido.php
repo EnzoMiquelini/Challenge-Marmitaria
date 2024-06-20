a3446b9b6d46e4ae4e5dc1ea5bbd003e310b4857
EnzoMiquelini <enzo.smiquelini@gmail.com>
2024-06-20 12:33:10 -0300

    .

--- pedido.php
+++ pedido.php
@@ -31,7 +31,33 @@
 
     <h1 class="text-center">Pedido</h1>
 
-
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col">Nome do Produto</th>
+            <th scope="col" colspan="3">Quantidade</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
 
     </div>
 
