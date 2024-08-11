88ab1e3f46a81ffc16a373e310636d3c5c79609c
EnzoMiquelini <enzo.smiquelini@gmail.com>
2024-08-11 00:18:30 -0300

    .

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <link rel="shortcut icon" href="img/food(1).png" type="image/x-icon">
     <title>Sabores Do Bem</title>
     
 </head>
@@ -14,7 +15,7 @@
     <div class="container d-flex">
         <aside class="row bg-black text-white align-items-center w-25 rounded-3 mt-3" style=" height: 96vh; ">
             <div class="col d-flex flex-column gap-3" >
-                <a href="pedido.php" class="btn btn-secondary w-100" id="novo_pedido" value="''">Novo Pedido</a>
+                <a href="pedido.php" class="btn btn-secondary w-100" id="novo_pedido">Novo Pedido</a>
                 <a href="Categoria_de_produtos.php" class="btn btn-secondary w-100 ">Produtos</a>
                 <!-- <a href="" class="btn btn-secondary w-100 ">Histórico Pedidos</a> -->
                 <a href="clientes.php" class="btn btn-secondary w-100 ">Clientes</a>
