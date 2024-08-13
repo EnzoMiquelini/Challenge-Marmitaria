b53451be88ca44c3bc6aef73a522197c2f894f7b
EnzoMiquelini <enzo.smiquelini@gmail.com>
2024-08-13 11:52:31 -0300

    .

--- clientes.php
+++ clientes.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <link rel="shortcut icon" href="img/food.png" type="image/x-icon">
     <title>Sabores Do Bem</title>
     
 </head>
