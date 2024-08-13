08a7ea6f0c3492a029a9af94573efbefa41ed381
EnzoMiquelini <enzo.smiquelini@gmail.com>
2024-08-13 11:56:28 -0300

    .

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/sidebar.css">
     <link rel="shortcut icon" href="img/food.png" type="image/x-icon">
     <title>Sabores Do Bem</title>
     
 </head>
 <body>
-    <div class="background d-flex">
+    <div class="d-flex">
 
         <aside class="aside bg-black text-white align-items-center rounded-4 mx-4 my-4">
             <div class="text-aside d-flex justify-content-center align-items-center gap-2 fs-5">
--- pedido.php
+++ pedido.php
@@ -10,12 +10,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
     <link rel="shortcut icon" href="img/food.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/sidebar.css">
     <title>Sabores Do Bem</title>
     
 </head>
 <body>
 
-    <div class="container">
+    <div class="d-flex">
 
         <div class="cadastro">
             <div class="d-flex justify-content-between">
